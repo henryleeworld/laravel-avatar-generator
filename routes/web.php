7b22d03af4375e23757c0b1041b7b4ade58e1dd6
Henry Lee <henryleeworld@gmail.com>
2020-09-15 20:08:21 +0800

    feat:升級至 laravel 8

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AvatarController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,4 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('avatar/output/', 'AvatarController@outputImage');
+Route::get('avatar/output/', [AvatarController::class, 'outputImage']);
